645c585cffc3cdaa17b5606563af2885e9ea15f7
Luka Bjelica <52993990+bjelicaluka@users.noreply.github.com>
2023-06-16 12:38:51 +0200

    feat: handling user delete event (#11)

---
+++ ftrip.io.booking-service/ftrip.io.booking-service/ReservationRequests/Consumers/GuestDeletedEventConsumer.cs
@@ -0,0 +1,31 @@
+﻿using ftrip.io.booking_service.ReservationRequests.UseCases.DeleteReservationRequestsByGuest;
+using ftrip.ip.user_service.contracts.Users.Events;
+using MassTransit;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ftrip.io.booking_service.ReservationRequests.Consumers
+{
+    public class GuestDeletedEventConsumer : IConsumer<UserDeletedEvent>
+    {
+        private readonly IMediator _mediator;
+
+        public GuestDeletedEventConsumer(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task Consume(ConsumeContext<UserDeletedEvent> context)
+        {
+            var userDeleted = context.Message;
+            if (userDeleted.UserType != "Guest")
+            {
+                return;
+            }
+
+            await _mediator.Send(new DeleteReservationRequestsByGuestRequest() { GuestId = Guid.Parse(userDeleted.UserId) }, CancellationToken.None);
+        }
+    }
+}
---
+++ ftrip.io.booking-service/ftrip.io.booking-service/ReservationRequests/UseCases/DeleteReservationRequestsByGuest/DeleteReservationRequestsByGuestRequest.cs
@@ -0,0 +1,12 @@
+﻿using ftrip.io.booking_service.ReservationRequests.Domain;
+using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace ftrip.io.booking_service.ReservationRequests.UseCases.DeleteReservationRequestsByGuest
+{
+    public class DeleteReservationRequestsByGuestRequest : IRequest<IEnumerable<ReservationRequest>>
+    {
+        public Guid GuestId { get; set; }
+    }
+}
